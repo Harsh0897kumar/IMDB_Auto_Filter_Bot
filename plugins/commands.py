e6eeb5c1fb1bc428141feed2ee4685585686c529
technical_aks <105633032+technicalaks@users.noreply.github.com>
2022-12-07 12:19:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -56,6 +56,10 @@ async def start(client, message):
             InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ ᴏᴜʀ ʏᴛ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@noharaadda')
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_chat_action(enums.ChatAction.TYPING)
+        m=await message.reply_sticker("CAACAgUAAxkBAAEGtDtjjwMDy2VxBDPUdthF75PKjolepgACNAYAAjDgeFTjDnvThZ_q6ysE")
+        await asyncio.sleep(1)
+        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
