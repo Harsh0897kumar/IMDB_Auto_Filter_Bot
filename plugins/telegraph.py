9a4835823097859ef28f4da2606b0907d1bfb466
technical_aks <105633032+technicalaks@users.noreply.github.com>
2022-12-08 19:40:25 +0530

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -1,1 +1,92 @@
+import os
+import shutil
+from pyrogram import Client, filters
+from telegraph import upload_file
+from pyrogram import (
+    filters
+)
+from pyrogram.types import Message
+from pyrogram.types.messages_and_media import message
 
+
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            "contact",
+            "dice",
+            "poll",
+            "location",
+            "venue",
+            "sticker"
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                setattr(obj, "message_type", message_type)
+                return obj
+
+
+USE_AS_BOT = os.environ.get("USE_AS_BOT", True)
+
+def onw_filter(filt, client, message):
+    if USE_AS_BOT:
+        return bool(
+            True # message.from_user.id in ADMINS
+        )
+    else:
+        return bool(
+            message.from_user and
+            message.from_user.is_self
+        )
+
+
+f_onw_fliter = filters.create(
+    func=onw_filter,
+    name="OnwFilter"
+)
+
+TMP_DOWNLOAD_DIRECTORY = "./DOWNLOADS/"
+
+@Client.on_message(
+    filters.command("telegraph") &
+    f_onw_fliter
+)
+async def telegraph(client, message):
+    replied = message.reply_to_message
+    if not replied:
+        await message.reply_text("𝚁𝙴𝙿𝙻𝚈 𝚃𝙾 𝙰 𝙿𝙷𝙾𝚃𝙾 𝙾𝚁 𝚅𝙸𝙳𝙴𝙾 𝚄𝙽𝙳𝙴𝚁 𝟻𝙼𝙱.")
+        return
+    file_info = get_file_id(replied)
+    if not file_info:
+        await message.reply_text("Not supported!")
+        return
+    _t = os.path.join(
+        TMP_DOWNLOAD_DIRECTORY,
+        str(replied.message_id)
+    )
+    if not os.path.isdir(_t):
+        os.makedirs(_t)
+    _t += "/"
+    download_location = await replied.download(
+        _t
+    )
+    try:
+        response = upload_file(download_location)
+    except Exception as document:
+        await message.reply_text(message, text=document)
+    else:
+        await message.reply(
+            f"Link :- <code>https://telegra.ph{response[0]}</code>\n\nLink :- <code>https://graph.org{response[0]}</code>",
+            disable_web_page_preview=True
+        )
+    finally:
+        shutil.rmtree(
+            _t,
+            ignore_errors=True
+        )
